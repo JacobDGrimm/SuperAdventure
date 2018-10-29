7b192286624977ab9ca0e66459e3f47dc12c97fa
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-29 15:52:44 -0400

    adds the constructor to the quest completion items class

--- Engine/QuestCompletionItem.cs
+++ Engine/QuestCompletionItem.cs
@@ -10,5 +10,11 @@ namespace Engine
     {
         public Item Details { get; set; }
         public int Quantity { get; set; }
+
+        public QuestCompletionItem(Item details, int quantity)
+        {
+            Details = details;
+            Quantity = quantity;
+        }
     }
 }
